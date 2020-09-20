5a2ab38ac255a7d35edeb8cfef63345ac02e06ec
Liuhancheng <cn_lhc@qq.com>
2020-09-20 21:18:32 +0800

    hooks can work

--- swrap/hooks.c
+++ swrap/hooks.c
@@ -2,9 +2,29 @@
 #include <sys/socket.h>
 #include <dlfcn.h>
 #include <stdlib.h>
+#include <pthread.h>
 
+#define _GNU_SOURCE
+
+#define INIT() init_lib_wrapper(__FUNCTION__)
 #define SETUP_SYM(X) do { if (! true_ ## X ) true_ ## X = load_sym( # X, X ); } while(0)
 
+pthread_once_t init_once = PTHREAD_ONCE_INIT;
+static void setup_hooks(void) ;
+
+
+static void do_init(void) {
+	srand(time(NULL));
+	setup_hooks();
+}
+
+static void init_lib_wrapper(const char* caller) {
+#ifndef DEBUG
+	(void) caller;
+#endif
+	//if(!init_l) PDEBUG("%s called from %s\n", __FUNCTION__,  caller);
+	pthread_once(&init_once, do_init);
+}
 static void* load_sym(char* symname, void* proxyfunc) {
 	void *funcptr = dlsym(RTLD_NEXT, symname);
 
@@ -22,8 +42,10 @@ static void* load_sym(char* symname, void* proxyfunc) {
 }
 typedef ssize_t (*sendto_t) (int sockfd, const void *buf, size_t len, int flags,
 			     const struct sockaddr *dest_addr, socklen_t addrlen);
+typedef ssize_t (*send_t) (int sockfd, const void *buf, size_t len, int flags);
 
 sendto_t true_sendto;
+send_t true_send;
 
 ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
 	       const struct sockaddr *dest_addr, socklen_t addrlen) {
@@ -32,7 +54,14 @@ ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
 }
 
 
+ssize_t send(int sockfd, const void *buf, size_t len, int flags){
+    INIT();
+    fprintf(stdout,"len:%d content:%s\n",len,buf);
+    return true_send(sockfd,buf,len,flags);
+}
 static void setup_hooks(void) {
 	SETUP_SYM(sendto);
+	SETUP_SYM(send);
 }
+
 
