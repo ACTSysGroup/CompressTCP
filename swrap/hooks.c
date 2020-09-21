a566cd56e890abc4c699fbfdec2a403e129e5042
CNLHC <2463765697@qq.com>
2020-09-21 21:27:37 +0800

    implement write wrap.(we can use iperf test now

--- swrap/hooks.c
+++ swrap/hooks.c
@@ -3,8 +3,8 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include "compress.h"
 
-#define _GNU_SOURCE
 
 #define INIT() init_lib_wrapper(__FUNCTION__)
 #define SETUP_SYM(X) do { if (! true_ ## X ) true_ ## X = load_sym( # X, X ); } while(0)
@@ -43,9 +43,13 @@ static void* load_sym(char* symname, void* proxyfunc) {
 typedef ssize_t (*sendto_t) (int sockfd, const void *buf, size_t len, int flags,
 			     const struct sockaddr *dest_addr, socklen_t addrlen);
 typedef ssize_t (*send_t) (int sockfd, const void *buf, size_t len, int flags);
+       
+typedef  ssize_t (*write_t)(int fd, const void *buf, size_t count);
+
 
 sendto_t true_sendto;
 send_t true_send;
+write_t true_write;
 
 ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
 	       const struct sockaddr *dest_addr, socklen_t addrlen) {
@@ -53,15 +57,25 @@ ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
     return 0 ;
 }
 
+char buffer[1<<29];
+
+ssize_t write(int fd, const void *buf, size_t count){
+    INIT();
+    int csize= compress_buffer(buf,count,buffer,count*2);
+    //fprintf(stdout,"write: raw %ld bytes\t compressed %d bytes\n",count,csize);
+    return true_write(fd,buf,count);
+}
+
 
 ssize_t send(int sockfd, const void *buf, size_t len, int flags){
     INIT();
-    fprintf(stdout,"len:%d content:%s\n",len,buf);
+    fprintf(stdout,"len:%ld content:%s\n",len,(char *) buf);
     return true_send(sockfd,buf,len,flags);
 }
 static void setup_hooks(void) {
 	SETUP_SYM(sendto);
 	SETUP_SYM(send);
+	SETUP_SYM(write);
 }
 
 
